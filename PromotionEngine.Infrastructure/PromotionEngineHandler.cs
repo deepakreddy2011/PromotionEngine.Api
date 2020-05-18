0cb9416f9d56d10579557838f5fb3a87268e8e01
Deepak Tumukunta <dtumukun@jcp.com>
2020-05-18 16:09:07 +0530

    added test class for handler where test are in red

---
+++ PromotionEngine.Infrastructure/PromotionEngineHandler.cs
@@ -0,0 +1,16 @@
+﻿using PromotionEngine.Core;
+using PromotionEngine.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Infrastructure
+{
+    public class PromotionEngineHandler: IPromotionEngineHandler
+    {
+        public int CalculateTotalPrice(List<Item> inputItemList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PromotionEngine.UnitTests/PromotionEngineHandlerTest.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using PromotionEngine.Core;
+using PromotionEngine.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.UnitTests
+{
+    [TestFixture]
+    public class PromotionEngineHandlerTest
+    {
+        [TestCase]
+        public void TestCalculateTotalPriceScenario1()
+        {
+            var inputItemList = new List<Item>();
+            inputItemList.Add(new Item { Id = "A", Quantity = 1 });
+            inputItemList.Add(new Item { Id = "B", Quantity = 1 });
+            inputItemList.Add(new Item { Id = "C", Quantity = 1 });
+
+            var handler = new PromotionEngineHandler();
+            var price = handler.CalculateTotalPrice(inputItemList);
+
+            Assert.AreEqual(price, 100);
+        }
+
+        [TestCase]
+        public void TestCalculateTotalPriceScenario2()
+        {
+            var inputItemList = new List<Item>();
+            inputItemList.Add(new Item { Id = "A", Quantity = 5 });
+            inputItemList.Add(new Item { Id = "B", Quantity = 5 });
+            inputItemList.Add(new Item { Id = "C", Quantity = 1 });
+
+
+            var handler = new PromotionEngineHandler();
+            var price = handler.CalculateTotalPrice(inputItemList);
+
+            Assert.AreEqual(price, 370);
+        }
+
+        [TestCase]
+        public void TestCalculateTotalPriceScenario3()
+        {
+            var inputItemList = new List<Item>();
+            inputItemList.Add(new Item { Id = "A", Quantity = 3 });
+            inputItemList.Add(new Item { Id = "B", Quantity = 5 });
+            inputItemList.Add(new Item { Id = "C", Quantity = 1 });
+            inputItemList.Add(new Item { Id = "D", Quantity = 1 });
+
+            var handler = new PromotionEngineHandler();
+            var price = handler.CalculateTotalPrice(inputItemList);
+
+            Assert.AreEqual(price, 280);
+        }
+    }
+}
