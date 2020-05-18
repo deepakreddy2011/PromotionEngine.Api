a011284b434d8d6b1627e0e05d97a2e848a65a55
Deepak Tumukunta <dtumukun@jcp.com>
2020-05-18 14:45:55 +0530

    added test class for PromotionEngine.Api class

---
+++ PromotionEngine.UnitTests/PromotionEngineApiTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.UnitTests
+{
+    [TestFixture]
+    public class PromotionEngineApiTest
+    {
+        [SetUp]
+        public void SetUp() 
+        {
+        }
+
+        [TestCase]
+        public void TestToGetTotalPrice() 
+        {
+
+        }
+
+    }
+}
